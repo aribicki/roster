4e6b24b0ffaf6c443c676a5b23717b5f813f52f8
aribicki <aribicki@saintjoe.edu>
2015-02-20 09:34:21 -0600

    Data set created for class

---
+++ roster.js
@@ -0,0 +1,27 @@
+var ROSTER = [
+  {"player":["29","Bryan Bickell","Left"]}
+  {"player":["13","Daniel Carcillo","Left"]}
+  {"player":["38","Ryan Hartman","Right"]}
+  {"player":["81","Marian Hossa","Right"]}
+  {"player":["88","Patrick Kane","Right"]}
+  {"player":["16","Marcus Kruger","Center"]}
+  {"player":["91","Brad Richards","Center"]}
+  {"player":["20","Brandon Saad","Left"]}
+  {"player":["10","Patrick Sharp","Left"]}
+  {"player":["65","Andrew Shaw","Center"]}
+  {"player":["28","Ben Smith","Right"]}
+  {"player":["19","Jonathan Toews","Center"]}
+  {"player":["23","Kris Versteeg","Left"]}
+  {"player":["26","Kyle Cumiskey","Def"]}
+  {"player":["34","Tim Erixon","Def"]}
+  {"player":["4","Niklas Hjalmarsson","Def"]}
+  {"player":["2","Duncan Keith","Def"]}
+  {"player":["27","Johnny Oduya","Def"]}
+  {"player":["32","Michal Rozsival","Def"]}
+  {"player":["5","David Rundblad","Def"]}
+  {"player":["7","Brent Seabrook","Def"]}
+  {"player":["57","Trevor Van Riemsdyk","Def"]}
+  {"player":["50","Corey Crawford","Goalie"]}
+  {"player":["31","Antti Raanta","Goalie"]}
+  {"player":["coach","Joel Quenneville","Head Coach"]}
+];
