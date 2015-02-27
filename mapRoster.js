1a5b18b786bb8b17fc63ef507e54208585cb6398
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 08:21:57 -0600

    finished map time to test

--- mapRoster.js
+++ mapRoster.js
@@ -7,3 +7,12 @@ load('roster.js');
 var roster = JSON.parse(ROSTER_FILE);
 function map(array, transform) {
  var mapped = [];
+ for (var i = 0; i < array.length: i++) {
+   mapped.push(transform(array[i]));
+ }
+  return mapped;
+}
+
+print(JSON.stringify(map(shootingpecentage, function(percentage) {
+  return Math.floor(percentage.sp *=2);
+})));
