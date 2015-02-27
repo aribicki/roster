0a8381f7d2f33e87b56728978a2e725df443abe8
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 13:13:31 -0600

    added missing commas

--- roster.js
+++ roster.js
@@ -1,28 +1,28 @@
 var ROSTER_FILE = [ 
-  '{"playernumber":"29", "name": "Bryan Bickell", "pos": "Left", "sp": "12.50"}' 
-  '{"playernumber":"13", "name": "Daniel Carcillo", "pos": "Left", "sp": "9.50"}'
-  '{"playernumber":"38", "name": "Ryan Hartman", "pos": "Right", "sp": "0.00"}'
-  '{"playernumber":"81", "name": "Marian Hossa", "pos": "Right", "sp": "9.40"}'
-  '{"playernumber":"88", "name": "Patrick Kane",  "pos": "Right", "sp": "5.90"}'
-  '{"playernumber":"16", "name": "Marcus Kruger", "pos": "Center", "sp": "5.30"}'
-  '{"playernumber":"91", "name": "Brad Richards", "pos": "Center", "sp": "5.90"}'
-  '{"playernumber":"20", "name": "Brandon Saad", "pos": "Left", "sp": "12.70"}'
-  '{"playernumber":"10", "name": "Patrick Sharp", "pos": "Left", "sp": "5.90"}'
-  '{"playernumber":"65", "name": "Andrew Shaw", "pos": "Center", "sp": "7.90"}'
-  '{"playernumber":"28", "name": "Ben Smith", "pos": "Right", "sp": "6.60"}'
-  '{"playernumber":"19", "name": "Jonathan Toews", "pos": "Center", "sp": "12.70"}'
-  '{"playernumber":"23", "name": "Kris Versteeg", "pos": "Left", "sp": "13.00"}'
-  '{"playernumber":"26", "name": "Kyle Cumiskey", "pos":  "Def", "sp": "0.00"}'
-  '{"playernumber":"34", "name": "Tim Erixon", "pos": "Def", "sp": "0.00"}'
-  '{"playernumber":"4", "name": "Niklas Hjalmarsson", "pos": "Def", "sp": "3.00"}'
-  '{"playernumber":"2", "name": Duncan Keith", "pos": " "Def", "sp": "5.30"}'
-  '{"playernumber":"27", "name": "Johnny Oduya", "pos": "Def", "sp": "3.10"}'
-  '{"playernumber":"32", "name": "Michal Rozsival", "pos": "Def", "sp": "2.40"}'
-  '{"playernumber":"5", "name": "David Rundblad", "pos": "Def", "sp": "6.10"}'
-  '{"playernumber":"7", "name": "Brent Seabrook", "pos": "Def", "sp": "4.90"}'
-  '{"playernumber":"57", "name": "Trevor Van Riemsdyk", "pos": "Def", "sp": "0.00"}'
-  '{"playernumber":"50", "name": "Corey Crawford", "pos": "Goalie", "sp": "0.00"}'
-  '{"playernumber":"31", "name": "Antti Raanta", "pos": "Goalie", "sp": "0.00"}'
-  '{"playernumber":"coach", "name": "Joel Quenneville", "pos": "Head Coach", "sp": "0.00"}'
+  '{"playernumber":"29", "name": "Bryan Bickell", "pos": "Left", "sp": "12.50"}', 
+  '{"playernumber":"13", "name": "Daniel Carcillo", "pos": "Left", "sp": "9.50"}',
+  '{"playernumber":"38", "name": "Ryan Hartman", "pos": "Right", "sp": "0.00"}',
+  '{"playernumber":"81", "name": "Marian Hossa", "pos": "Right", "sp": "9.40"}',
+  '{"playernumber":"88", "name": "Patrick Kane",  "pos": "Right", "sp": "5.90"}',
+  '{"playernumber":"16", "name": "Marcus Kruger", "pos": "Center", "sp": "5.30"}',
+  '{"playernumber":"91", "name": "Brad Richards", "pos": "Center", "sp": "5.90"}',
+  '{"playernumber":"20", "name": "Brandon Saad", "pos": "Left", "sp": "12.70"}',
+  '{"playernumber":"10", "name": "Patrick Sharp", "pos": "Left", "sp": "5.90"}',
+  '{"playernumber":"65", "name": "Andrew Shaw", "pos": "Center", "sp": "7.90"}',
+  '{"playernumber":"28", "name": "Ben Smith", "pos": "Right", "sp": "6.60"}',
+  '{"playernumber":"19", "name": "Jonathan Toews", "pos": "Center", "sp": "12.70"}',
+  '{"playernumber":"23", "name": "Kris Versteeg", "pos": "Left", "sp": "13.00"}',
+  '{"playernumber":"26", "name": "Kyle Cumiskey", "pos":  "Def", "sp": "0.00"}',
+  '{"playernumber":"34", "name": "Tim Erixon", "pos": "Def", "sp": "0.00"}',
+  '{"playernumber":"4", "name": "Niklas Hjalmarsson", "pos": "Def", "sp": "3.00"}',
+  '{"playernumber":"2", "name": Duncan Keith", "pos": " "Def", "sp": "5.30"}',
+  '{"playernumber":"27", "name": "Johnny Oduya", "pos": "Def", "sp": "3.10"}',
+  '{"playernumber":"32", "name": "Michal Rozsival", "pos": "Def", "sp": "2.40"}',
+  '{"playernumber":"5", "name": "David Rundblad", "pos": "Def", "sp": "6.10"}',
+  '{"playernumber":"7", "name": "Brent Seabrook", "pos": "Def", "sp": "4.90"}',
+  '{"playernumber":"57", "name": "Trevor Van Riemsdyk", "pos": "Def", "sp": "0.00"}',
+  '{"playernumber":"50", "name": "Corey Crawford", "pos": "Goalie", "sp": "0.00"}',
+  '{"playernumber":"31", "name": "Antti Raanta", "pos": "Goalie", "sp": "0.00"}',
+  '{"playernumber":"coach", "name": "Joel Quenneville", "pos": "Head Coach", "sp": "0.00"}',
   ];
 
