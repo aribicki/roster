0d24e8e1bd68dcb775df80f39ed7a7ec6de66c80
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 08:30:14 -0600

    went back and added comments

--- filterRoster.js
+++ filterRoster.js
@@ -4,14 +4,14 @@
 // data filter
 
 
-load('roster.js');
+load('roster.js'); // loading data set 
 
-var roster = JSON.parse(ROSTER_FILE);
+var roster = JSON.parse(ROSTER_FILE);  // setting data set = var roster
 
 function filter(array, test) {
   var passed = []
-  for (var i = 0; i < array.length; i++) {
-    if (test(array[i])) {
+  for (var i = 0; i < array.length; i++) {           // have rough idea of what this is doing but pulled it from our book
+    if (test(array[i])) {                            // STUDY ME!
      passed.push(array[i]);
      }
      }
@@ -19,6 +19,6 @@ function filter(array, test) {
      return passed;
      }
      
-     print(JSON.stringify(filter(playernumbers, function(number) {
-       return number.playernumber < 50;
+     print(JSON.stringify(filter(playernumbers, function(number) {    // using filter to return all the player numbers under 50
+       return number.playernumber < 50;                               // then could use separate function to show the names
      })));
