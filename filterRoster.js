977e37f2537a52df291efbda2214ff71de65f06a
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 07:36:01 -0600

    working on code

---
+++ filterRoster.js
@@ -0,0 +1,22 @@
+// Andrew Ribicki
+// CMP 237
+// 2/27/2015
+// data filter
+
+
+load('roster.js');
+
+var roster = JSON parse(ROSTER_FILE);
+
+function filter(array, test) {
+  var passed = []
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i])) {
+     passed.push(array[i]);
+     }
+     }
+     }
+     return passed;
+     }
+     
+     print(JSON.stringify(filter
