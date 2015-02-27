728f3e424fc504c27a523fd6f4b54e36a625d918
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 08:32:23 -0600

    went back and added comments

--- mapRoster.js
+++ mapRoster.js
@@ -2,17 +2,17 @@
 // CMP 237 2/27/2015
 // map multiplying % (shooting percentage)
 
-load('roster.js');
+load('roster.js');  //loading data set 
 
-var roster = JSON.parse(ROSTER_FILE);
+var roster = JSON.parse(ROSTER_FILE); // using data set as var roster
 function map(array, transform) {
  var mapped = [];
- for (var i = 0; i < array.length: i++) {
+ for (var i = 0; i < array.length: i++) {                   // STUDY ME! FOR BETTER UNDERSTANDING!
    mapped.push(transform(array[i]));
  }
   return mapped;
 }
 
-print(JSON.stringify(map(shootingpecentage, function(percentage) {
-  return Math.floor(percentage.sp *=2);
-})));
+print(JSON.stringify(map(shootingpecentage, function(percentage) {   //pulling shooting percentage and then x2 to show greater 
+  return Math.floor(percentage.sp *=2);                              // distance/easier to understand player performance
+})));                                                                // compared to rest of team
