a8ea78fdb8d9577c3c4a40a266f81969f40729c9
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 07:57:46 -0600

    fixing data set

--- roster.js
+++ roster.js
@@ -1,27 +1,27 @@
 var ROSTER_FILE = "[\n " + [
-  {"player":["29","Bryan Bickell","Left","12.50"]}
-  {"player":["13","Daniel Carcillo","Left","9.50"]}
-  {"player":["38","Ryan Hartman","Right","0.00"]}
-  {"player":["81","Marian Hossa","Right","9.40"]}
-  {"player":["88","Patrick Kane","Right","5.90"]}
-  {"player":["16","Marcus Kruger","Center","5.30"]}
-  {"player":["91","Brad Richards","Center","5.90"]}
-  {"player":["20","Brandon Saad","Left","12.70"]}
-  {"player":["10","Patrick Sharp","Left","5.90"]}
-  {"player":["65","Andrew Shaw","Center","7.90"]}
-  {"player":["28","Ben Smith","Right","6.60"]}
-  {"player":["19","Jonathan Toews","Center","12.70"]}
-  {"player":["23","Kris Versteeg","Left","13.00"]}
-  {"player":["26","Kyle Cumiskey","Def","0.00"]}
-  {"player":["34","Tim Erixon","Def","0.00"]}
-  {"player":["4","Niklas Hjalmarsson","Def","3.00"]}
-  {"player":["2","Duncan Keith","Def","5.30"]}
-  {"player":["27","Johnny Oduya","Def","3.10"]}
-  {"player":["32","Michal Rozsival","Def","2.40"]}
-  {"player":["5","David Rundblad","Def","6.10"]}
-  {"player":["7","Brent Seabrook","Def","4.90"]}
-  {"player":["57","Trevor Van Riemsdyk","Def","0.00"]}
-  {"player":["50","Corey Crawford","Goalie","0.00"]}
-  {"player":["31","Antti Raanta","Goalie","0.00"]}
-  {"player":["coach","Joel Quenneville","Head Coach","0.00"]}
+  {"playernumber":"29", "name": "Bryan Bickell", "pos": "Left","12.50"}
+  {"playernumber":"13", "name": "Daniel Carcillo", "pos": "Left","9.50"}
+  {"playernumber":"38", "name": "Ryan Hartman", "pos": "Right","0.00"}
+  {"playernumber":"81", "name": "Marian Hossa", "pos": "Right","9.40"}
+  {"playernumber":"88", "name": "Patrick Kane",  "pos": "Right","5.90"}
+  {"playernumber":"16", "name": "Marcus Kruger", "pos": "Center","5.30"}
+  {"playernumber":"91", "name": "Brad Richards", "pos": "Center","5.90"}
+  {"playernumber":"20", "name": "Brandon Saad", "pos": "Left","12.70"}
+  {"playernumber":"10", "name": "Patrick Sharp", "pos": "Left","5.90"}
+  {"playernumber":"65", "name": "Andrew Shaw", "pos": "Center","7.90"}
+  {"playernumber":"28", "name": "Ben Smith", "pos": "Right","6.60"}
+  {"playernumber":"19", "name": "Jonathan Toews", "pos": "Center","12.70"}
+  {"playernumber":"23", "name": "Kris Versteeg", "pos": "Left","13.00"}
+  {"playernumber":"26", "name": "Kyle Cumiskey", "pos":  "Def","0.00"}
+  {"playernumber":"34", "name": "Tim Erixon", "pos": "Def","0.00"}
+  {"playernumber":"4", "name": "Niklas Hjalmarsson", "pos": "Def","3.00"}
+  {"playernumber":"2", "name": Duncan Keith", "pos": " "Def", "5.30"}
+  {"playernumber":"27", "name": "Johnny Oduya", "pos": "Def","3.10"}
+  {"playernumber":"32", "name": "Michal Rozsival", "pos": "Def","2.40"}
+  {"playernumber":"5", "name": "David Rundblad", "pos": "Def","6.10"}
+  {"playernumber":"7", "name": "Brent Seabrook", "pos": "Def","4.90"}
+  {"playernumber":"57", "name": "Trevor Van Riemsdyk", "pos": "Def","0.00"}
+  {"playernumber":"50", "name": "Corey Crawford", "pos": "Goalie","0.00"}
+  {"playernumber":"31", "name": "Antti Raanta", "pos": "Goalie","0.00"}
+  {"playernumber":"coach", "name": "Joel Quenneville", "pos": "Head Coach","0.00"}
 ].join(",\n ") + "\n]";;
