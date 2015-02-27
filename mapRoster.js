d6e8d26d90e836cb33c770b37fb41d1b61659cf7
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 08:14:50 -0600

    working on map. saving.

---
+++ mapRoster.js
@@ -0,0 +1,9 @@
+// Andrew Ribicki
+// CMP 237 2/27/2015
+// map multiplying % (shooting percentage)
+
+load('roster.js');
+
+var roster = JSON.parse(ROSTER_FILE);
+function map(array, transform) {
+ var mapped = [];
